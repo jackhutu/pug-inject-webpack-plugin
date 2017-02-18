f5b472c068600c47f0dcacc28ea722caa2050cb8
Jack Hu <hutufeia2009@sina.com>
2017-02-18 23:50:40 +0800

    update to v0.2.0

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ function regexMatchAll(content, replaceContent, indentContent, type) {
   if(type === 'css'){
     REGEX = /(\/\/css\sinject)([\s\S]*?)(\/\/end\sinject)/ig
   }
-  return content.replace(REGEX, '$1\n' + indentContent + replaceContent + '\n'+ indentContent + '$3')
+  //去掉原来的标记
+  return content.replace(REGEX, replaceContent + '\n'+ indentContent)
+  //加上原为的标记
+  //return content.replace(REGEX, '$1\n' + indentContent + replaceContent + '\n'+ indentContent + '$3')
 }
 
-function makeTags(file, assets, type) {
+function makeTags(file, assets, type, hash) {
   var tags = ''
   assets.map(function (i) {
-    if(i.substr(-4) !== '.map' && i.indexOf(file) !== -1){
-      if(type === 'js'){
+    if(type === 'js'){
+      //是否有hash符都匹配
+      if(i.substr(-4) !== '.map' && (i === 'js/' + file + '.' + hash + '.js' || i === 'js/' + file + '.js')){
         tags += 'script(type="text/javascript" src="' + path.normalize(i) + '")'
-      }else if(type === 'css'){
-        tags += 'link(rel="stylesheet", href="' + path.normalize(i) + '")'
       }
+    }else if(type === 'css'){
+      if(i.substr(-4) !== '.map' && (i === 'styles/' + file + '.' + hash + '.css' || i === 'styles/' + file + '.css')){
+        tags += 'link(rel="stylesheet", href="' + path.normalize(i) + '")'
+      }        
     }
   })
   return tags
@@ -44,19 +50,24 @@ function PugInjectPlugin(options) {
   }
 }
 
-PugInjectPlugin.prototype.pugInject = function (assets) {
+PugInjectPlugin.prototype.pugInject = function (compilation) {
+  var assets = Object.keys(compilation.assets)
+  var hash = compilation.hash.substring(0,8)
   var content = fs.readFileSync(this.options.filePath, 'utf-8')
   var indentContent = this.indentContent
   //如果不需要注入则直接写入到output
-  if(!this.options.inject){
-    fs.writeFileSync(this.options.output, content, 'utf-8')
-  }else{
+  if(this.options.inject){
     //js注入处理
     if(this.options.injectJs && this.options.injectJs.length > 0){
       var jsLinks = [];
+      //获取要注入的文件名
       this.options.injectJs.forEach(function (file) {
-        var filename = path.basename(file,'.js') + '.js';
-        jsLinks.push(makeTags(filename,assets,'js'))
+        //加上hash字符串
+        var filename = path.basename(file,'.js')
+        var jsTags = makeTags(filename,assets,'js', hash)
+        if(jsTags !== ''){
+          jsLinks.push(jsTags)
+        }
       })
       content = regexMatchAll(content,jsLinks.join('\n' + indentContent),indentContent)
     }
@@ -65,19 +76,36 @@ PugInjectPlugin.prototype.pugInject = function (assets) {
       var cssLinks = [];
       this.options.injectCss.forEach(function (file) {
         //配置加不加扩展名都可以
-        var filename = path.basename(file,'.css') + '.css';
-        cssLinks.push(makeTags(filename,assets,'css'))
+        var filename = path.basename(file,'.css')
+        var cssTags = makeTags(filename,assets,'css', hash)
+        if(cssTags !== ''){
+          cssLinks.push(cssTags)
+        }
       })
       content = regexMatchAll(content,cssLinks.join('\n' + indentContent),indentContent,'css')
     }
-    fs.writeFileSync(this.options.output, content, 'utf-8')
   }
+  var dirname = path.dirname(this.options.output)
+  if(!fs.existsSync(dirname)){
+    //console.log(path.dirname(this.options.output) + '文件夹不存在,需要创建')
+    fs.mkdirSync(dirname)
+  }
+  fs.writeFileSync(this.options.output, content, 'utf-8')
+  //写入文件, 这种写入方式在内存中, express获取不到
+  // compilation.assets[this.options.output] = {
+  //   source: function() {
+  //     return content;
+  //   },
+  //   size: function() {
+  //     return content.length;
+  //   }
+  // }
 }
 
 PugInjectPlugin.prototype.apply = function (compiler) {
   var _this = this
   compiler.plugin('emit', function (compilation,callback) {
-    _this.pugInject(Object.keys(compilation.assets))
+    _this.pugInject(compilation)
     callback()
   })
 }
