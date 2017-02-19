23302801b7373d063bb36933ca2e12481d270b59
Jack Hu <hutufeia2009@sina.com>
2017-02-19 16:13:13 +0800

    add images extract

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+'use strict'
+
 var fs = require('fs')
 var path = require('path')
+var _ = require('lodash')
 
 function regexMatchAll(content, replaceContent, indentContent, type) {
   var REGEX = /(\/\/js\sinject)([\s\S]*?)(\/\/end\sinject)/ig
@@ -42,7 +45,7 @@ function getIndentContent(indent){
 }
 
 function PugInjectPlugin(options) {
-  if(options && typeof(options) === 'object' && options.filePath && options.output){
+  if(options && typeof(options) === 'object' && options.filePath && options.outputFilePath){
     this.options = Object.assign({indent:['spaces',2]},options)
     this.indentContent = getIndentContent(this.options.indent)
   }else{
@@ -50,11 +53,13 @@ function PugInjectPlugin(options) {
   }
 }
 
-PugInjectPlugin.prototype.pugInject = function (compilation) {
+PugInjectPlugin.prototype.pugInject = function (compilation,compiler) {
   var assets = Object.keys(compilation.assets)
   var hash = compilation.hash.substring(0,8)
   var content = fs.readFileSync(this.options.filePath, 'utf-8')
   var indentContent = this.indentContent
+  //images
+  this.extractImages(content,compilation)
   //如果不需要注入则直接写入到output
   if(this.options.inject){
     //js注入处理
@@ -85,12 +90,12 @@ PugInjectPlugin.prototype.pugInject = function (compilation) {
       content = regexMatchAll(content,cssLinks.join('\n' + indentContent),indentContent,'css')
     }
   }
-  var dirname = path.dirname(this.options.output)
+  var dirname = path.dirname(this.options.outputFilePath)
   if(!fs.existsSync(dirname)){
     //console.log(path.dirname(this.options.output) + '文件夹不存在,需要创建')
     fs.mkdirSync(dirname)
   }
-  fs.writeFileSync(this.options.output, content, 'utf-8')
+  fs.writeFileSync(this.options.outputFilePath, content, 'utf-8')
   //写入文件, 这种写入方式在内存中, express获取不到
   // compilation.assets[this.options.output] = {
   //   source: function() {
@@ -101,23 +106,82 @@ PugInjectPlugin.prototype.pugInject = function (compilation) {
   //   }
   // }
 }
+/**
+ * extract images
+ */
+PugInjectPlugin.prototype.extractImages = function(content,compilation){
+  var _this = this
+  //匹配图片
+  var REGX = /img\(.*?src=("|')(.+?)("|').*\)/ig
+  var httpREGX = /^(https|http|ftp|rtsp|mms)/i
+  var imageREGX = /(jpe?g|gif|png)$/i
+  var result
+  _this.images = []
+  while ((result = REGX.exec(content)) != null)  {
+    var imageSrc = result[2]
+    if(imageREGX.test(imageSrc) && !httpREGX.test(imageSrc)){
+      //加入
+      _this.images.push(imageSrc)
+      var originSrcUrl = path.resolve(this.options.filePath.split('views')[0], imageSrc)
+      //var outputSrcUrl = path.resolve(this.options.output, imageSrc)
+      var stat = fs.statSync(originSrcUrl)
+      if(stat.isFile()){
+        compilation.assets[imageSrc] = {
+          source: function() {
+            return fs.readFileSync(originSrcUrl)
+          },
+          size: function() {
+            return stat.size
+          }
+        }
+      }
 
+    }
+  }
+}
 /**
  * add file to watch
  */
-PugInjectPlugin.prototype.addWatch = function(compilation){
+PugInjectPlugin.prototype.addWatch = function(filePath,compilation){
   //fileDependencies,contextDependencies
-  compilation.fileDependencies.push(this.options.filePath)
+  if (_.includes(compilation.fileDependencies, filePath)) {
+    //console.log('文件已经加入 fileDependencies')
+  } else {
+    //console.log('add to fileDependencies' + filePath)
+    compilation.fileDependencies.push(filePath)
+  }
+  if (_.includes(compilation.contextDependencies, filePath)) {
+    //console.log('文件已经加入 contextDependencies')
+  } else {
+    //console.log('add to contextDependencies' + filePath)
+    compilation.contextDependencies.push(filePath)
+  }  
 }
 
 PugInjectPlugin.prototype.apply = function (compiler) {
   var _this = this
+  var globalRef = {
+    context: compiler.options.context,
+    output: compiler.options.output.path,
+  }
+  if (globalRef.output === '/' &&
+    compiler.options.devServer &&
+    compiler.options.devServer.outputPath) {
+    globalRef.output = compiler.options.devServer.outputPath
+  }
+  _this.options = Object.assign(globalRef,_this.options)
+  
   compiler.plugin('emit', function (compilation,callback) {
-    _this.pugInject(compilation)
+    _this.pugInject(compilation,compiler)
     callback()
   })
   compiler.plugin('after-emit', function (compilation,callback) {
-    _this.addWatch(compilation)
+    _this.addWatch(_this.options.filePath,compilation)    
+    _.forEach(_this.images, (file) => {
+      if(file){
+        _this.addWatch(file,compilation)
+      }
+    })
     callback()
   })  
 }
